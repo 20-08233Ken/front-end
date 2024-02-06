b59648115bfc11d1294af762054d3caae120479f
Ken <20-08233@g.batstate-u.edu.ph>
2024-02-06 17:04:09 +0800

    Planning Office V1

---
+++ src/components/Home/Planning/graph/graph3.vue
@@ -0,0 +1,114 @@
+<template>
+    
+    <v-chart class="chart" :option="option" autoresize />
+    
+  </template>
+  
+  <script setup>
+  import { use } from 'echarts/core';
+  import { CanvasRenderer } from 'echarts/renderers';
+  import { PieChart } from 'echarts/charts';
+  import "echarts";
+  import {
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+  } from 'echarts/components';
+  import VChart, { THEME_KEY } from 'vue-echarts';
+  import { ref, provide } from 'vue';
+  
+  use([
+    CanvasRenderer,
+    PieChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+  ]);
+  
+  provide(THEME_KEY, 'white');
+  
+  const option = {
+    title:{
+    text:"Top 10 Colleges",
+    top: "15%",
+    left:"65%",
+    textStyle: {
+      fontSize:"20px",
+      color:"#94080D"
+    }
+  },
+  legend: {
+      type:"scroll",
+      orient:"vertical",
+      top:"20%",
+      left:"65%",
+      textStyle: {
+        fontSize:"14px",
+    } 
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)'
+  },
+  color: [
+  '#CF0412',
+  '#E86217',
+  '#CF7E04',
+  '#2DCF04',
+  '#04CF9E',
+  '#04AACF',
+  '#CF0472',
+  '#8E04CF',
+  '#0431CF',
+  '#70F5ED'
+
+  ],
+  toolbox: {
+    show: true,
+    feature: {
+      mark: { show: true },
+      // dataView: { show: true, readOnly: false },
+      // restore: { show: true },
+      // saveAsImage: { show: true }
+    }
+  },
+  series: [
+    {
+      name: 'College',
+      type: 'pie',
+      radius: [100, 300],
+      center: ['30%', '50%'],
+      roseType: 'area',
+      itemStyle: {
+        borderRadius: 8
+      },
+      data: [
+        { value: 105, name: 'College of Accountancy, Business, Economics, and International Hospitality Management' },
+        { value: 100, name: 'College of Engineering, Architecture, and Fine Arts' },
+        { value: 95, name: 'College of Arts and Sciences' },
+        { value: 85, name: 'College of Teacher Education' },
+        { value: 80, name: 'College of Industrical Technology' },
+        { value: 75, name: 'College of Informatics and Computing Sciences' },
+        { value: 70, name: 'College of Nursing' },
+        { value: 65, name: 'College of Agriculture' },
+        { value: 60, name: 'College of Law' },
+        { value: 55, name: 'College of College of Medicine' },
+      ] 
+    }
+  ]
+};
+  </script>
+
+    
+<style scoped>
+.chart {
+  /* height: 50rem; */
+  
+  width: 100%;
+  background-color: white;
+  padding: 4  rem 0;
+
+
+ 
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,15 @@
    import Header from "../components/Home/Header.vue"
    import Subnav2 from "../components/Home/Subnav2.vue";
     import Landing from "../components/Home/Dean/Landing.vue";
-
+    
     import PlanningProfile from '../components/Home/Planning/Profile.vue'
+    import PlanningLanding from '../components/Home/Planning/Landing.vue'
 </script>
 
 <template>
     <div id = "home-main">
         <Header></Header>
-        <PlanningProfile/>
+        <PlanningLanding/>
 
             </div>
     
